d00900e49a830c31c9b8c7501bdcdf204efaacd7
hashercat <95425480+hashercat@users.noreply.github.com>
2021-12-19 10:22:29 -0300

    Add treasury to simpleSplitter

--- contracts/simpleSplitter.sol
+++ contracts/simpleSplitter.sol
@@ -16,6 +16,7 @@ contract SimpleSplitter {
     address public immutable satoku;
     address public immutable hashercat;
     address public immutable saito;
+    address public immutable treasury;
 
     event SplitBalanceBetweenAddress(
         address _okinaPrime,
@@ -24,6 +25,8 @@ contract SimpleSplitter {
         uint256 _satokuSplit,
         address _hashercat,
         uint256 _hashercatSplit,
+        address _treasury,
+        uint256 _treasurySplit,
         address _saito,
         uint256 _saitoSplit
     );
@@ -33,35 +36,42 @@ contract SimpleSplitter {
         address _okinaPrime,
         address _satoku,
         address _hashercat,
-        address _saito
+        address _saito,
+        address _treasury
     ) {
         avaone = _avaone;
         okinaPrime = _okinaPrime;
         satoku = _satoku;
         hashercat = _hashercat;
         saito = _saito;
+        treasury = _treasury;
     }
 
     function splitBalanceBetweenAddress() external {
         require(avaone.balanceOf(address(this)) != 0, "Cannot split a balance of zero!");
-        uint256 thirtyTwoPercent = avaone.balanceOf(address(this)).mul(320).div(1000);
+        uint256 thirtyPercent = avaone.balanceOf(address(this)).mul(300).div(1000);
+        uint256 sixPercent = avaone.balanceOf(address(this)).mul(60).div(1000);
 
-        // 32% for okina
-        avaone.safeTransfer(okinaPrime, thirtyTwoPercent);
-        // 32% for satoku
-        avaone.safeTransfer(satoku, thirtyTwoPercent);
-        // 32% for hashercat
-        avaone.safeTransfer(hashercat, thirtyTwoPercent);
+        // 30% for okina
+        avaone.safeTransfer(okinaPrime, thirtyPercent);
+        // 30% for satoku
+        avaone.safeTransfer(satoku, thirtyPercent);
+        // 30% for hashercat
+        avaone.safeTransfer(hashercat, thirtyPercent);
+        // 6% for treasury.
+        avaone.safeTransfer(treasury, sixPercent);
         // Remaining (4%) for saito
         uint256 saitoSplit = avaone.balanceOf(address(this));
         avaone.safeTransfer(saito, saitoSplit);
         emit SplitBalanceBetweenAddress(
             okinaPrime,
-            thirtyTwoPercent,
+            thirtyPercent,
             satoku,
-            thirtyTwoPercent,
+            thirtyPercent,
             hashercat,
-            thirtyTwoPercent,
+            thirtyPercent,
+            treasury,
+            sixPercent,
             saito,
             saitoSplit
         );
